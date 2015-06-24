c5e171cd2dd789cf127b4f587f40fbe17e590a25
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-06-24 16:49:35 +0800

    Added some important package information in package.json

--- app.js
+++ app.js
@@ -46,17 +46,17 @@ var RedisStore          = require('connect-redis')(session);
 // lodash 常用函数库
 var _                   = require('lodash');
 // 验证用的模块，必须在 session 模块之后引用
-var csurf               = require('csurf');
+//var csurf               = require('csurf');
 // http 压缩用的模块（deflate，gzip）
-var compression         = require('compression');
+//var compression         = require('compression');
 // 用于解析请求体
 var bodyParser          = require('body-parser');
 // 用于大文件上传
-var busboy              = require('connect-busboy');
+//var busboy              = require('connect-busboy');
 // 用于在开发环境下打印错误信息
 var errorhandler        = require('errorhandler');
 // 用于支持 CORS 跨域
-var cors                = require('cors');
+//var cors                = require('cors');
 var requestLog          = require('./middlewares/request_log');
 var renderMiddleware    = require('./middlewares/render');
 var logger              = require('./common/logger');
@@ -70,14 +70,14 @@ var staticDir   = path.join(__dirname, 'public');
 var assets      = {};
 
 // 非 debug 模式下开启压缩
-if (config.mini_assets) {
-    try {
-        assets = require('./assets.json');
-    } catch (e) {
-        console.log('You must execute `make build` before start app when mini_assets is true.');
-        throw e;
-    }
-}
+// if (config.mini_assets) {
+//     try {
+//         assets = require('./assets.json');
+//     } catch (e) {
+//         console.log('You must execute `make build` before start app when mini_assets is true.');
+//         throw e;
+//     }
+// }
 
 var urlinfo     = require('url').parse(config.host);
 config.hostname = urlinfo.hostname || config.host;
@@ -85,6 +85,7 @@ config.hostname = urlinfo.hostname || config.host;
 var app         = express();
 
 // 配置环境变量
+// 视图/模板引擎相关
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs-mate'));
@@ -105,3 +106,17 @@ if (config.debug) {
 app.use(Loader.less(__dirname));
 app.use('/public', express.static(staticDir));
 app.use('/agent', proxyMiddleware.proxy);
+
+// 每日访问限制
+
+app.use(require('response-time')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// 添加额外的支持头，如 PUT DELETE
+//app.use(require('method-override')());
+app.use(require('cookie-parser')(config.session_secret));
+app.use(compression());
+app.use(session({
+    secret: config.session_secret
+}));
+
