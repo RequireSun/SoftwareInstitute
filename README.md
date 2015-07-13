04aa43050a7788a9c12d2d9750b072c377901f80
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-07-14 01:30:48 +0800

    fixed middleware bugs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SoftwareInstitute
 A Node.js CMS using MySQL.
+
+TODO:
+1. request_log 中间件的 ip 获取方式 
--- middlewares/request_log.js
+++ middlewares/request_log.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var logger = require('../common/logger');
+// var app = require('../app');
 
 module.exports = function (req, res, next) {
     // 静态资源请求不记录
@@ -10,16 +11,18 @@ module.exports = function (req, res, next) {
         return;
     }
 
+    // sign 获取 ip
     // 判断是否使用代理， 并获取真实的客户地址
-    var reqIp = app.enabled('trust proxy')? req.ips[0]: req.ip;
+    // var reqIp = app.enabled('trust proxy')? req.ips[0]: req.ip;
+    var reqIp = req.ip;
     // 开始结束计时
     var time = new Date();
     logger.log('\nStarted', time.toUTCString(), req.method, req.url, reqIp);
 
     res.on('finish', function () {
-        var duration ((new Date()) - time);
+        var duration = ((new Date()) - time);
 
-        logger.log('Completed', res.statusCode, ('(' + furation + 'ms)').green, req.method, req.url, reqIp);
+        logger.log('Completed', res.statusCode, ('(' + duration + 'ms)').green, req.method, req.url, reqIp);
     });
 
     next();
