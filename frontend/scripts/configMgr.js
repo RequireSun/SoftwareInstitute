496267f2fb7d8fc0fb0a6fed2faa2c5b6891e896
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-09-28 20:36:45 +0800

    management page

---
+++ frontend/scripts/configMgr.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kelvinsun on 2015/9/28.
+ */
+define([], function () {
+    return {
+        sections: [{
+            index: 'index',
+            name: '首页'
+        }, {
+            index:'news',
+            name: '新闻通知'
+        }, {
+            index: 'resource',
+            name: '资源下载'
+        }]
+    };
+});
--- frontend/scripts/viewMgr/main.jsx
+++ frontend/scripts/viewMgr/main.jsx
@@ -1,24 +1,69 @@
-define(['react', 'ReactRouter', 'react-bootstrap'/*, 'view/public'*/], function (React, Router, ReactBootstrap/*, templatePublic*/) {
-    //var Navigation = templatePublic.Navigation,
-    //    Footer = templatePublic.Footer;
-    var RouteHandler = Router.RouteHandler;
-    var Row = ReactBootstrap.Row,
-        Col = ReactBootstrap.Col;
+'use strict';
 
-    var Main = React.createClass({
-        render: function () {
+define(['react', 'ReactRouter', 'react-bootstrap', 'root/configMgr'], function (React, Router, ReactBootstrap, configMgr) {
+    var RouteHandler = Router.RouteHandler;
+    var Link = Router.Link;
+    var Navbar = ReactBootstrap.Navbar,
+        Nav = ReactBootstrap.Nav;
+
+    var sections = configMgr.sections || [];
+
+    class MgrNav extends React.Component {
+        constructor (props) {
+            super(props);
+            this.state = props;
+        }
+
+        componentWillReceiveProps (nextProps) {
+            this.setState(nextProps);
+        }
+
+        render () {
+            return (
+                <Navbar brand="后台管理" fixedTop inverse toggleNavKey={0} className="visible-xs-block">
+                    <Nav right eventKey={0}>
+                        {this.state.sections.map(
+                            (item) =>
+                                (<li key={item.index}><Link to={item.index}>{item.name}</Link></li>)
+                        )}
+                    </Nav>
+                </Navbar>
+                );
+        }
+    }
+    MgrNav.propTypes = { section: React.PropTypes.array };
+    MgrNav.defaultProps = { section: [] };
+
+    class Main extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = props;
+        }
+
+        static willTransitionTo(transition) {
+            console.log(transition);
+        }
+
+        render () {
             return (
                 <div className="container-fluid">
+                    <MgrNav/>
+                    <div className="row" className="visible-xs-block" style={{ height: 50 }}></div>
                     <div className="row">
-                        <div className="col-sm-2 col-md-1 hidden-xs">
-                            123
+                        <div className="col-sm-3 col-lg-2 hidden-xs" style={{ backgroundColor: 'red' }}>
+                            <ListGroup>
+                                {sections.map((item)=>(<li className="list-group-item" key={item.index}><Link to={item.index}>{item.name}</Link></li>))}
+                            </ListGroup>
                         </div>
-                        <RouteHandler/>
+                        <div className="col-sm-9 col-md-10" style={{ backgroundColor: 'green' }}>
+                            321
+                            <RouteHandler/>
+                        </div>
                     </div>
                 </div>
             );
         }
-    });
+    }
 
     return Main;
 });
