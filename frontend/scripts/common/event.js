198d1cbc0012f51a2f5df6decd7c6a35af3695b5
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-10-23 18:08:48 +0800

    add comments on event.js todo namespace of event.js

--- frontend/scripts/common/event.js
+++ frontend/scripts/common/event.js
@@ -1,9 +1,14 @@
 /**
  * Created by kelvinsun on 2015/10/23.
  */
-
 define(['jquery'], function ($) {
     var EventDom = $('<script>');
+    /**
+     * 事件绑定
+     * @param events    事件名
+     * @param handler   事件处理函数
+     * @param namespace 命名空间, 防止误删用
+     */
     function on (events, handler, namespace) {
         EventDom.on(events, function () {
             var args = Array.prototype.slice.call(arguments),
@@ -12,14 +17,24 @@ define(['jquery'], function ($) {
             handler && handler.apply(null, args);
         });
     }
+    /**
+     * 事件触发
+     * @param events    事件名
+     */
     function emit (events) {
         var args = Array.prototype.slice.call(arguments);
         args.shift();
         EventDom.trigger(events, args);
     }
+    /**
+     * 事件解绑
+     * @param events    事件名
+     * @param namespace 命名空间, 防止误删用
+     */
     function off (events, namespace) {
         EventDom.off(events);
     }
+
     return {
         on: on,
         off: off,
