b1be19d35f8a973ad22036b1a8b494f2601e87a4
Kelvin-09 <kelvinlpsun@hotmail.com>
2015-10-26 18:20:48 +0800

    deleted drag supports for news management

--- frontend/scripts/viewMgr/news.jsx
+++ frontend/scripts/viewMgr/news.jsx
@@ -22,7 +22,6 @@ define(['react', 'ReactRouter', 'root/configMgr', 'common/event', 'action/news']
         }
     }
 
-    // 看起来只能拿 dragenter 来做了
     class NewsStructureItem extends React.Component {
         constructor (props) {
             super(props);
@@ -33,35 +32,36 @@ define(['react', 'ReactRouter', 'root/configMgr', 'common/event', 'action/news']
             this.setState(nextProps);
         }
 
-        dragStart (itemId, event) {
-            Event.emit('management.news.drag.start', this.state.id, itemId, event);
-        }
-
-        dragEnd () {
-            Event.emit('management.news.drag.end');
-        }
-
-        dragEnter (event) {
-            Event.emit('management.news.drag.enter', this.state.id, event);
-        }
-
         render () {
-            var tempCategories = this.state.categories;
-            var categoryElements = [];
+            var tempCategories = this.state.categories, tempOutlines = this.state.outlines;
+            var categoryElements = [], outlineElements = [];
             for (var i in tempCategories) {
-                categoryElements.push(<a href="javascript:;" onDragStart={this.dragStart.bind(this, tempCategories[i])} className="list-group-item">{i}</a>);
+                if (tempCategories.hasOwnProperty(i)) {
+                    categoryElements.push(<a href="javascript:;" className="list-group-item">{i}</a>);
+                }
             }
+            for (var i in tempOutlines) {
+                outlineElements.push(<button type="button" className="list-group-item">{i}</button>);
+            }
+            var popUpStyle = {
+                position: 'absolute',
+                right: 0,
+                top: 0
+            };
             return (
-                <div className="panel panel-default" onDragEnd={this.dragEnd.bind(this)} onDragEnter={this.dragEnter.bind(this)}>
+                <div className="panel panel-default" style={{ position: 'relative' }}>
                     <div className="panel-heading">{this.state.name}</div>
                     <div className="list-group">
                         {categoryElements}
+                    </div>
+                    <div className="list-group" style={popUpStyle}>
+                        {outlineElements}
                     </div>
                 </div>
             );
         }
     }
-    NewsStructureItem.defaultProps = { id: 0, name: '', categories: {} };
+    NewsStructureItem.defaultProps = { id: 0, name: '', categories: {}, outlines: {} };
 
     class NewsStructure extends React.Component {
         constructor (props) {
@@ -74,41 +74,6 @@ define(['react', 'ReactRouter', 'root/configMgr', 'common/event', 'action/news']
         }
 
         componentWillMount () {
-            var draggedSource = 0,
-                draggedTarget = 0,
-                draggedItemId = 0;
-            var intervalKey, canMove = false;
-            var domElem;
-            Event.on('management.news.drag.start', function (sourceId, itemId, event) {
-                draggedSource = sourceId;
-                draggedTarget = sourceId;
-                draggedItemId = itemId;
-
-                domElem = $('<div>123</div>');
-                domElem.css({
-                    'position': 'fixed',
-                    'top': event.screenY,
-                    'left': event.screenX
-                });
-                domElem.appendTo(document.body);
-                intervalKey = setInterval(function () {
-                    canMove = true;
-                }, 50);
-            });
-            Event.on('management.news.drag.end', function () {
-                console.log(draggedSource, draggedTarget, draggedItemId);
-                clearInterval(intervalKey);
-            });
-            Event.on('management.news.drag.enter', function (enterId, event) {
-                if (canMove) {
-                    domElem.css({
-                        'top': event.screenY,
-                        'left': event.screenX
-                    });
-                    canMove = false;
-                }
-                draggedTarget = enterId;
-            });
             actionNews.OutlineCategory(function (err, data) {
                 if (err) {
                     location.hash = '#notFound/' + err;
@@ -121,17 +86,13 @@ define(['react', 'ReactRouter', 'root/configMgr', 'common/event', 'action/news']
         }
 
         componentWillUnmount () {
-            Event.off('management.news.drag.start');
-            Event.off('management.news.drag.end');
-            Event.off('management.news.drag.enter');
-            Event.off('management.news.drag.leave');
         }
 
         render () {
             var tempOutlines = this.state.outlines;
             var outlineElements = [];
             for (var i in tempOutlines) {
-                outlineElements.push(<NewsStructureItem id={tempOutlines[i].id} name={i} categories={tempOutlines[i].category}/>);
+                outlineElements.push(<NewsStructureItem id={tempOutlines[i].id} name={i} categories={tempOutlines[i].category} outlines={this.state.outlines}/>);
             }
             return (
                 <div>
