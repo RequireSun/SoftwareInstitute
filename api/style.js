09d00521d8ca81a0f4fbedd0f3f01fb7530800f9
kelvinsun(孙承祚) <kelvinsun@tencent.com>
2016-04-18 03:54:20 +0800

    add get style

--- api/style.js
+++ api/style.js
@@ -1,10 +1,30 @@
 /**
  * Created by kelvinsun on 2016/4/18.
  */
+'use strict';
 
 const Style = require('../proxy/style');
+const promiseWrap = require('../common/tool').promiseWrap;
 
-exports.get = (req, res, next) => {};
+exports.get = (req, res, next) => {
+    let name = req.query.name;
+    
+    if ('string' !== typeof name || !name)  {
+        res.jsonErrorParameterMissing('请传入样式名！');
+        next();
+        return;
+    }
+
+    new Promise(promiseWrap(Style.get, name)).
+        then(result => {
+            res.jsonSuccess(result);
+            next();
+        }).
+        catch(err => {
+            res.jsonErrorParameterWrong(err['message']);
+            next();
+        });
+};
 
 exports.post = (req, res, next) => {};
 
--- proxy/style.js
+++ proxy/style.js
@@ -1,20 +1,45 @@
 /**
  * Created by kelvinsun on 2016/4/18.
  */
+'use strict';
 
-const fs = require('fs');
+const fs = require('fs'),
+      path = require('path');
+const promiseWrapTail = require('../common/tool').promiseWrapTail;
 
 exports.get = (callback, name) => {
     if ('string' !== typeof name || !name) {
         return callback(new Error('Parameter: name must be string!'));
     }
+    const filePath = path.join(__dirname, '../style/main.json');
 
-    fs.readFile('../style/main.json', {
-        encoding: 'utf-8',
-        flag    : 'r',
-    }, (err, data) => {
-        if (err) { callback(err); } else { callback(null, data); }
-    });
+    new Promise((resolve, reject) => {
+        fs.exists(filePath, exists => {
+            if (!exists) {
+                fs.writeFile(filePath, '', {
+                    encoding: 'utf-8',
+                    flag    : 'w',
+                }, err => {
+                    if (err) {
+                        reject('Create file failed!');
+                    } else {
+                        resolve();
+                    }
+                });
+            } else {
+                resolve();
+            }
+        });
+    }).then(() => new Promise(promiseWrapTail(
+        fs.readFile,
+        filePath, {
+            encoding: 'utf-8',
+            flag    : 'r',
+        })
+    )).then(result =>
+        callback(null, result)
+    ).catch(err => callback(err));
+
 };
 
 exports.post = (callback, name, style) => {};
